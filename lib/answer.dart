201a74b54126730ca2c6b902fe0588067c688ca6
Shah Vaibhav <vaibhavshah@Vaibhavs-MacBook-Air.local>
2021-04-25 15:30:25 +0530

    answer button implemented

---
+++ lib/answer.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+
+  Answer(this.selectHandler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(primary: Colors.deepPurple),
+        child: Text('Answer 1'),
+        onPressed: this.selectHandler,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 // void main() {
 //   runApp(MyApp());
 // }
@@ -8,7 +9,6 @@ import './question.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -16,7 +16,7 @@ class MyApp extends StatefulWidget {
   }
 }
 
-class _MyAppState extends State<MyApp>{
+class _MyAppState extends State<MyApp> {
   var questionIndex = 0;
 
   void _answerQuestion() {
@@ -26,20 +26,23 @@ class _MyAppState extends State<MyApp>{
   }
 
   Widget build(BuildContext context) {
-
+    var questions = ['What\'s divya nick name?', 'What\'s kevin nick name?'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First Flutter App'),
+          title: Text('My First App'),
           centerTitle: true,
           backgroundColor: Color(0xff00BCD1),
         ),
         body: Column(
           children: [
-            Question(questionIndex),
-            ElevatedButton(child: Text('Option 1'), onPressed: _answerQuestion),
-            ElevatedButton(child: Text('Option 2'), onPressed: _answerQuestion),
-            ElevatedButton(child: Text('Option 3'), onPressed: _answerQuestion),
+            Question(questions[questionIndex]),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            // ElevatedButton(child: Text('Option 1'), onPressed: _answerQuestion),
+            // ElevatedButton(child: Text('Option 2'), onPressed: _answerQuestion),
+            // ElevatedButton(child: Text('Option 3'), onPressed: _answerQuestion),
           ],
         ),
       ),
